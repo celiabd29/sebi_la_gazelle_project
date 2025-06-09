6c62bb04167e2c1ccf1e37bab537dd9d16c39dce
MohamedEleisawy <eleisawy19@gmail.com>
2025-06-09 11:30:30 +0200

    ...

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,7 @@ MongoClient.connect(process.env.MONGO_URI)
     // ✅ Middlewares
     // ✅ Configuration CORS précise (à placer avant les routes)
     const corsOptions = {
+      origin: process.env.FRONTEND_URL,
       methods: ["GET", "POST", "PUT", "DELETE"],
       allowedHeaders: ["Content-Type", "Authorization"],
       credentials: true // Si vous utilisez des cookies/sessions
